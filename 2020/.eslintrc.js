e4261ff8ced0b5c70ac9e15c0dcf22fe2ec370d0
Bryce Hipp <stiffhipp@gmail.com>
2020-12-04 10:55:47 -0600

    Clean up eslintrc

--- 2020/.eslintrc.js
+++ 2020/.eslintrc.js
@@ -1,25 +1,19 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   root: true,
   env: {
     es6: true,
     node: true,
+    jest: true,
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
-    sourceType: "module",
+    sourceType: 'module',
   },
-  extends: ["airbnb-base", "prettier"],
-  plugins: ["json", "prettier"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    // Only allow debugger in development
-    // "no-debugger": isProd ? "error" : "off",
-    // Only allow `console.log` in development
-    // "no-console": isProd ? ["error", { allow: ["warn", "error"] }] : "off",
-
-    // "no-param-reassign": [2, { props: false }],
-    "import/prefer-default-export": 0,
-    "prettier/prettier": "error",
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    'prettier/prettier': 'error',
   },
-};
+}

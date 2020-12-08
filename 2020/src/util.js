5f3a6810e3b383aec89877b6de78f29765066708
Bryce Hipp <stiffhipp@gmail.com>
2020-12-08 10:10:39 -0600

    Add a way to get the raw file data

--- 2020/src/util.js
+++ 2020/src/util.js
@@ -1,13 +1,13 @@
 import fs from 'fs'
 import path from 'path'
 
-export async function getInput(day) {
+export async function getInput(day, split = true) {
   const inputPath = path.join(__dirname, `day${day}/input.txt`)
   try {
     const fileData = await fs.promises.readFile(inputPath, {
       encoding: 'UTF-8',
     })
-    return fileData.split('\n')
+    return split ? fileData.split('\n') : fileData
   } catch (err) {
     console.error(err)
     return []
